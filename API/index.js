7680f8cd007489c39b86083d3ebf6cabb1427c20
= <=>
2017-12-21 23:55:52 -0300

    Startup do projeto AluGO

---
+++ API/index.js
@@ -0,0 +1,23 @@
+//Importações
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import server from "./server";
+import routes from './routes';
+
+//Uso do express
+const app = express();
+
+//Definições de middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+//Chamada ao servidor
+server(app);
+
+//Definição das rotas
+routes(app);
---
+++ API/server.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+export default (app) => {
+    //Conexão com o MongoDB
+    let db = mongoose.connect('mongodb://localhost:27017/AluGO',
+        {
+            useMongoClient: true
+        },
+        function (err) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log('MongoDB conectado com sucesso!');
+            }
+        });
+
+    //Criação do servidor
+    app.listen(3000, () => {
+        console.log('Servidor express iniciado com sucesso!');
+    })
+
+}
