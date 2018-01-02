47d497c20b6c91ae58388b55ff11d8b0d24cdd98
= <=>
2018-01-02 18:13:13 -0300

    Inclusão do modelo do header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+//Components
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header/>
+          <Router>
+            <div>
+              <Route></Route>
+            </div>
+          </Router>
       </div>
     );
   }
