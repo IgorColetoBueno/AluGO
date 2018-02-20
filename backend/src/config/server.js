8d78aa80b169bc9efb5151be9d021530b6b133a7
= <=>
2018-02-20 19:49:44 -0400

    Preparação para o deploy

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export default (app) => {
     //Conexão com o MongoDB
-    let db = mongoose.connect('mongodb://localhost:27017/AluGO',
+    let db = mongoose.connect('mongodb://dbalugo:8+8AwChE[_5cV@mongo_alugo:27017:27017/alugo',
         {
             useMongoClient: true
         },
@@ -17,7 +17,7 @@ export default (app) => {
 
     mongoose.Promise = global.Promise;
 
-    const port = process.env.PORT || 3000;
+    const port =   3000||process.env.PORT;
     //Criação do servidor
     app.listen(port, (err) => {
         if (err) {
